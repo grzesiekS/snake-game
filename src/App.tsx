a35f871a30a7455f2cc696f5ba0232c160866b5b
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-04-28 18:05:55 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import { MainLayout } from './components/layout/MainLayout/MainLayout';
 import { HomePage } from './components/views/HomePage/HomePage';
 
 import './styles/global.scss';
 
 const App: React.FC = () => (
-  <div>
+  <BrowserRouter>
     <MainLayout>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </MainLayout>
-  </div>
+  </BrowserRouter>
 );
 
 export default App;
