12981903fbbd01bb25bdb045d4f3735ae6b2df73
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-04-29 19:30:10 +0200

    Implement NavBar in Game component

--- src/components/features/Game/Game.tsx
+++ src/components/features/Game/Game.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { NavBar } from '../NavBar/NavBar';
 import { Board } from './Board/Board';
 
 import styles from './Game.module.scss';
 
+const linksData = [
+  {
+    _id: '1',
+    text: 'Start Game',
+    url: '/game',
+  },
+  {
+    _id: '2',
+    text: 'Score List',
+    url: '/scores',
+  },
+];
+
 export const Game: React.FC = () => (
   <div className={styles.container}>
+    <NavBar links={linksData} />
     <Board />
   </div>
 );
