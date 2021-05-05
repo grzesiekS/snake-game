95658bf40fdada33550e86b8df689cac8b47092b
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-05 15:59:28 +0200

    Modify Splash component and add clsx package

--- src/components/features/Splash/Splash.tsx
+++ src/components/features/Splash/Splash.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Button } from '../../common/Button/Button';
+import { Title } from '../../common/Title/Title';
 
 import styles from './Splash.module.scss';
 
 export const Splash: React.FC = ({}) => (
   <div className={styles.container}>
-    <h1 className={styles.title}>Snake Game</h1>
+    <Title 
+      text='Snake Game'
+      type='large'
+    />
     <div className={styles.control}>
       <Button Type='Link' to='/game' text={'Start Game'} />
       <Button text={'Score List'} />
