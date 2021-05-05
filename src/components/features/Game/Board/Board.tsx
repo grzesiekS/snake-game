2220e647168bdc88a71191e8968c1fd9593135d7
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-05 17:14:03 +0200

    Create board and add styles for this component

--- src/components/features/Game/Board/Board.tsx
+++ src/components/features/Game/Board/Board.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-
-import { Title } from '../../../common/Title/Title';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import styles from './Board.module.scss';
 
 export const Board: React.FC = () => {
+
+  const [ boardArray, setBoardArray ] = useState<Array<Array<number>>>([]);
+  const [ rowSize ] = useState<number>(21);
+  const [ columnSize ] = useState<number>(35);
+
+  useEffect(() => {
+    const array = Array.from(Array(rowSize).fill(Array(columnSize).fill(0)));
+    setBoardArray(array);
+  }, [columnSize, rowSize]);
+
   return (
     <div className={styles.container}>
-      <Title text='Snake' type='small' />
+      <div className={styles.board}>
+        { boardArray.map( ( element ) => (
+          <div key={uuidv4()} className={styles.row}>
+            {element.map(block => (
+              <div 
+                key={uuidv4()} 
+                className={styles.block}
+              >
+                {block}
+              </div>
+            ))}
+          </div>
+        )) }
+      </div>
     </div>
   );
 };
