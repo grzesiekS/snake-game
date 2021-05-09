c459527ef1ca3f765333b9c5ea98841d3166af02
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-09 13:23:35 +0200

    Add handle snake position and modify Array creation in Board component

--- src/components/features/Game/Board/Board.tsx
+++ src/components/features/Game/Board/Board.tsx
@@ -1,19 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import styles from './Board.module.scss';
 
-export const Board: React.FC = () => {
+interface BoardProps {
+  snakePosition: number[][],
+}
+
+export const Board: React.FC<BoardProps> = ({ snakePosition }) => {
 
   const [ boardArray, setBoardArray ] = useState<Array<Array<number>>>([]);
   const [ rowSize ] = useState<number>(21);
   const [ columnSize ] = useState<number>(35);
 
+  const handleSnakePosition = useCallback((boardArray) => {
+    const result = boardArray;
+
+    snakePosition.forEach(position => {
+      result[position[0]][position[1]] = 1;
+    });
+
+    return result;
+  },[snakePosition]);
+
   useEffect(() => {
-    const array = Array.from(Array(rowSize).fill(Array(columnSize).fill(0)));
-    setBoardArray(array);
-  }, [columnSize, rowSize]);
+    const array:number[][] = [];
+    for(let i = 0; i < rowSize; i++) {
+      array[i] = [];
+      for(let j = 0; j < columnSize; j++) {
+        array[i][j] = 0;
+      }
+    }
+    setBoardArray(handleSnakePosition(array));
+  }, [columnSize, handleSnakePosition, rowSize, snakePosition]);
 
   return (
     <div className={styles.container}>
