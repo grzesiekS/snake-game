9c8f0e68f89c2af93580922d1fcc929c779464c6
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-19 12:39:24 +0200

    Add interface for Score subcomponent

--- src/components/features/Game/Score/Score.tsx
+++ src/components/features/Game/Score/Score.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Title } from '../../../common/Title/Title';
 
 import styles from './Score.module.scss';
 
-export const Score: React.FC = () => (
-  <div className={styles.container}>
-    <Title text='Score:' type='small'/>
-    <Title text='1520' type='small'/>
-  </div>
-);
+interface ScoreProps {
+  score: number,
+  snakeLength: number,
+  setNewScore: (newScore:number) => void;
+}
+
+export const Score: React.FC<ScoreProps> = ({score, snakeLength, setNewScore}) => {
+  
+  useEffect(() => {
+    setNewScore(snakeLength * 10 - 10);
+  }, [setNewScore, snakeLength]);
+  
+  return (
+    <div className={styles.container}>
+      <Title text='Score:' type='small'/>
+      <Title text={`${score}`} type='small'/>
+    </div>
+  );
+};
