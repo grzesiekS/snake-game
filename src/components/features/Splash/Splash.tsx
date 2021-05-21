dc939684e57ec2bb676068781cd7481164ebb6ee
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-21 11:06:53 +0200

    Implement Input component in Splash

--- src/components/features/Splash/Splash.tsx
+++ src/components/features/Splash/Splash.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../../common/Button/Button';
 import { Title } from '../../common/Title/Title';
+import { Input } from '../../common/Input/Input';
 
 import styles from './Splash.module.scss';
 
-export const Splash: React.FC = ({}) => (
-  <div className={styles.container}>
-    <Title 
-      text='Snake Game'
-      type='large'
-    />
-    <div className={styles.control}>
-      <Button Type='Link' to='/game' text={'Start Game'} />
-      <Button text={'Score List'} />
+export const Splash: React.FC = ({}) => {
+  const [playerName, setPlayerName] = useState('Player Name');
+
+  return (
+    <div className={styles.container}>
+      <Title 
+        text='Snake Game'
+        type='large'
+      />
+      <Input
+        id='playerName'
+        labelText='Player Name'
+        value={playerName}
+        onChangeFunc={name => setPlayerName(name)}
+      />
+      <div className={styles.control}>
+        <Button Type='Link' to='/game' text={'Start Game'} />
+        <Button text={'Score List'} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
