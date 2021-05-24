bf7fdff02d61d99b875d842542a729c7dd455a04
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-24 15:23:44 +0200

    Implement Redirect in Splash component

--- src/components/features/Splash/Splash.tsx
+++ src/components/features/Splash/Splash.tsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../../common/Button/Button';
 import { Title } from '../../common/Title/Title';
 import { Input } from '../../common/Input/Input';
 
 import styles from './Splash.module.scss';
+import { Redirect } from 'react-router';
 
 export const Splash: React.FC = ({}) => {
   const [playerName, setPlayerName] = useState('Player Name');
+  const [startGame, setStartGame] = useState(false);
+
+  const handleStartGame = ():void => {
+    if(playerName && playerName.length <= 50) {
+      setStartGame(true);
+    }
+  };
+
+  useEffect(() => (
+    setStartGame(false)
+  ),[]);
 
   return (
     <div className={styles.container}>
@@ -23,9 +35,13 @@ export const Splash: React.FC = ({}) => {
         />
       </div>
       <div className={styles.control}>
-        <Button Type='Link' to='/game' text={'Start Game'} />
+        <Button 
+          text={'Start Game'}
+          OnClickFunc={() => handleStartGame()}
+        />
         <Button text={'Score List'} />
       </div>
+      {startGame && <Redirect to='/game' />}
     </div>
   );
 };
