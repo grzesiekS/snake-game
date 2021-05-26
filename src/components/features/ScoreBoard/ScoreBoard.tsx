6c76c2a4240c709307b7c971f79ca4efb96d94e1
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-26 11:12:07 +0200

    Add Title component to ScoreBoard

--- src/components/features/ScoreBoard/ScoreBoard.tsx
+++ src/components/features/ScoreBoard/ScoreBoard.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { linksData } from '../../../data/data';
 
 import { NavBar } from '../NavBar/NavBar';
+import { Title } from '../../common/Title/Title';
 
 import styles from './ScoreBoard.module.scss';
 
 export const ScoreBoard: React.FC = () => (
   <div className={styles.container}>
     <NavBar links={linksData} />
+    <Title text='Score List' type='medium' />
   </div>
 );
