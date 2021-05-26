a4c9800bd1a371d6753da54dcbdcffcfbe309f4b
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-26 10:40:59 +0200

    Create data file

---
+++ src/data/data.ts
@@ -0,0 +1,17 @@
+export const linksData = [
+  {
+    _id: '1',
+    text: 'Start Game',
+    url: '/game',
+  },
+  {
+    _id: '2',
+    text: 'Score List',
+    url: '/scores',
+  },
+];
+
+export const boardSize = {
+  rows: 21,
+  columns: 35,
+};
