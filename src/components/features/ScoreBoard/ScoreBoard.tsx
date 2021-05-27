071cf60de7ca72d2ee6fc30f97713e872dad5ea6
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-05-27 14:17:19 +0200

    Implement Modal for removing scores data in ScoreBoard component

--- src/components/features/ScoreBoard/ScoreBoard.tsx
+++ src/components/features/ScoreBoard/ScoreBoard.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { linksData } from '../../../data/data';
 
 import { NavBar } from '../NavBar/NavBar';
 import { Title } from '../../common/Title/Title';
 import { ScoreList } from './ScoreList/ScoreList';
 import { Button } from '../../common/Button/Button';
+import { Modal } from '../../common/Modal/Modal';
 
 import styles from './ScoreBoard.module.scss';
 
-export const ScoreBoard: React.FC = () => (
-  <div className={styles.container}>
-    <NavBar links={linksData} />
-    <Title text='Score List' type='medium' />
-    <ScoreList />
-    <div className={styles.controlPanel}>
-      <Button text='Reset Score List' />
-    </div>
-  </div>
-);
+export const ScoreBoard: React.FC = () => {
+  const [removeScoresModal, setRemoveScoresModal] = useState(false);
+
+  return (
+    <Modal
+      title='Do you want to remove all scores?'
+      leftButtonText='Yes'
+      leftButtonAction={() => {console.log('test');}}
+      rightButtonText='No'
+      rightButtonAction={() => setRemoveScoresModal(false)}
+      showModal={removeScoresModal}
+    >
+      <div className={styles.container}>
+        <NavBar links={linksData} />
+        <Title text='Score List' type='medium' />
+        <ScoreList />
+        <div className={styles.controlPanel}>
+          <Button 
+            text='Reset Score List'
+            OnClickFunc={() => setRemoveScoresModal(true)}
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+};
