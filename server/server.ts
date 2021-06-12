6cd7a6ad1e1e533d970a7b38c02d34f83fd31496
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-12 14:08:42 +0200

    Add server code in server.ts

---
+++ server/server.ts
@@ -0,0 +1,18 @@
+import express = require('express');
+import path = require('path');
+
+const app = express();
+
+app.use(express.json());
+
+/* REACT WEBSITE */
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+/* START SERVER */
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running ${port}`);
+});
